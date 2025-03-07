a6ae49c05c05555b0378a92223dc21c1d78a30e3
JiiahaoXU <535846269@qq.com>
2025-03-06 23:05:58 -0800

    update code

--- src/run.sh
+++ src/run.sh
@@ -1,4 +1,4 @@
 device_num=0
 seed=0
 
-CUDA_VISIBLE_DEVICES=$device_num python federated.py --data cifar10 --aggr tramark --seed $seed --alpha 0.02
+CUDA_VISIBLE_DEVICES=$device_num python federated.py --aggr tramark --data cifar10 --alpha 0.02 --k 0.01 --non_iid --gamma 0.5
